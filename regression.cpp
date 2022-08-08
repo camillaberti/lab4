2e0fe1038190bc13eda6b8ba1a0f8bf7a2033d27
Camilla Berti <camillaberti18@gmail.com>
2022-08-08 16:12:04 +0200

    Use vector of 2D points in Regression

--- regression.cpp
+++ regression.cpp
@@ -2,28 +2,35 @@
 
 #include <stdexcept>
 
-int Regression::size() const { return N_; }
+int Regression::size() const { return points_.size(); }
 
 void Regression::add(double x, double y) {
-  ++N_;
-  sum_x_ += x;
-  sum_y_ += y;
-  sum_xy_ += x * y;
-  sum_x2_ += x * x;
+  points_.push_back({x,y});
 }
 
 Result Regression::fit() const {
-  if (N_ < 2) {
+  int N = size();
+  double sum_x_{0.};
+  double sum_y_{0.};
+  double sum_xy_{0.};
+  double sum_x2_{0.};
+  for(auto const& p : points_){
+    sum_x_ += p.x;
+    sum_y_ += p.y;
+    sum_xy_ += p.x * p.y;
+    sum_x2_ += p.x * p.x;
+  }
+  if (N < 2) {
     throw std::runtime_error{"Not enough points to run a fit"};
   }
 
-  double const d = N_ * sum_x2_ - sum_x_ * sum_x_;
+  double const d = N * sum_x2_ - sum_x_ * sum_x_;
   if (d == 0.) {
     throw std::runtime_error{"Trying to fit a vertical line"};
   }
 
   double const a = (sum_y_ * sum_x2_ - sum_x_ * sum_xy_) / d;
-  double const b = (N_ * sum_xy_ - sum_x_ * sum_y_) / d;
+  double const b = (N * sum_xy_ - sum_x_ * sum_y_) / d;
 
   return {a, b};
 }
